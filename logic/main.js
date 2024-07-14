c4883e3d2f654b85bf04dda57a0ef3ea44d2b1d3
hassnaa <hassnaamahmoud96@gmail.com>
2024-07-15 00:41:05 +0200

    data

--- logic/main.js
+++ logic/main.js
@@ -1,22 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const resultsContainer = document.getElementById("results");
-      data.forEach((item) => {
-        const resultDiv = document.createElement("div");
-        resultDiv.classList.add(item.category.toLowerCase());
-
-        resultDiv.innerHTML = `
-            <div>
-              <img src="${item.icon}" alt="${item.category}" />
-              <h3>${item.category}</h3>
-            </div>
-            <p><span>${item.score}</span> / 100</p>
-          `;
-
-        resultsContainer.appendChild(resultDiv);
-      });
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-});
+    const data = [
+      {
+        "category": "Reaction",
+        "score": 80,
+        "icon": "./assets/images/icon-reaction.svg"
+      },
+      {
+        "category": "Memory",
+        "score": 92,
+        "icon": "./assets/images/icon-memory.svg"
+      },
+      {
+        "category": "Verbal",
+        "score": 61,
+        "icon": "./assets/images/icon-verbal.svg"
+      },
+      {
+        "category": "Visual",
+        "score": 72,
+        "icon": "./assets/images/icon-visual.svg"
+      }
+    ];
+  
+    const resultsContainer = document.getElementById("results");
+    data.forEach((item) => {
+      const resultDiv = document.createElement("div");
+      resultDiv.classList.add(item.category.toLowerCase());
+  
+      resultDiv.innerHTML = `
+        <div>
+          <img src="${item.icon}" alt="${item.category}" />
+          <h3>${item.category}</h3>
+        </div>
+        <p><span>${item.score}</span> / 100</p>
+      `;
+  
+      resultsContainer.appendChild(resultDiv);
+    });
+  });
+  
